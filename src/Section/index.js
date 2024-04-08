6f0345ff8e0c052e07b0679331fb0189614c8fa0
Dmytro Artiuk <dimairak88@wp.pl>
2024-04-08 17:05:58 +0200

    replaced heder na header

--- src/Section/index.js
+++ src/Section/index.js
@@ -2,10 +2,10 @@ import "./style.css";
 
 const Section = ({ title, body, extraHeaderContent }) => (
   <section className="section">
-    <heder className="section__header">
+    <header className="section__header">
       <h2 className="section__title">{title}</h2>
       {extraHeaderContent}
-    </heder>
+    </header>
     <div className="section__body">
       {body}
     </div>
