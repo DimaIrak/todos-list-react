d7435f63c02155ac659c202e7c7bb4fddf154c7a
Dmytro Artiuk <dimairak88@wp.pl>
2024-05-29 00:13:56 +0200

    added object theme with colors and breakpoint

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export const theme = {
+    color: {
+        lightGray: "#ddd",
+        white: "#fff",
+        forestGreen: "#228b22",
+        razzmatazz: "#e72c70",
+        gallery: "#eee",
+        teal: "#008080",
+        silver: "#ccc",
+    },
+    breakpoint: {
+        mobileMax: 767,
+    },
+};
