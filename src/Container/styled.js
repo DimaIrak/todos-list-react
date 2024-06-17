111b04434ce74c369b4f21daae324662950abe26
Dmytro Artiuk <dimairak88@wp.pl>
2024-06-17 10:34:07 +0200

    implementation styled components to Container component

---
+++ src/Container/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    font-family: 'Lato', sans-serif;
+    max-width: 900px;
+    padding: 25px;
+    margin: 0 auto;
+`;
