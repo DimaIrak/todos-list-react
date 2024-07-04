c6e169b65b7354ab12bd4008101a5a27ad661633
Dmytro Artiuk <dimairak88@wp.pl>
2024-07-04 20:00:46 +0200

    delete styled.js from component Container

--- src/Container/styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Wrapper = styled.div`
-    font-family: 'Lato', sans-serif;
-    max-width: 900px;
-    padding: 25px;
-    margin: 0 auto;
-`;
