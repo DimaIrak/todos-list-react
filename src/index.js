6479e1c1574f274e257afb57c0d693a78ccf5055
Dmytro Artiuk <dimairak88@wp.pl>
2024-07-24 13:33:37 +0200

    application split into features

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './GlobalStyle';
 import { theme } from "./theme";
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
